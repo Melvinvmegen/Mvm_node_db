2efa159c675f0e9f08b149e3a7d43c03dd7b949d
Melvin van Megen <melvin.vmegen@gmail.com>
2021-10-04 21:06:21 +0200

    UP (auth) refacto && error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,68 @@
 const User = require('../models/user')
-const { validationResult } = require('express-validator/check')
+const { validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-exports.signUp = (req, res, next) => {
+exports.signUp = async (req, res, next) => {
   const errors =  validationResult(req)
-  console.log(errors)
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed !')
     error.statusCode = 422
     error.data = errors.array
-    throw error
+    next(error)
+    return
   }
+  const email = req.body.email
   const password = req.body.password
-  bcrypt.hash(password, 12)
-    .then(hashedPassword => {
-      User.create({
-        email: req.body.email,
-        password: hashedPassword
-      })
-        .then(user => res.status(201).json({message: 'User successfully created!', user: user}))
-    })
-    .catch(error => console.log(error))
+
+  try {
+    let user = await User.findOne({ where: { email: email }})
+    if (user) {
+      const error = new Error('A user with this email already exists !')
+      error.statusCode = 422
+      next(error)
+      return
+    }
+    const hashedPassword = await bcrypt.hash(password, 12)
+    user = await User.create({ email: req.body.email, password: hashedPassword }) 
+    res.status(201).json({message: 'User successfully created!', user: user})
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error)
+  }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
   let loadedUser;
-  User.findOne({ where: { email: email }})
-  .then(user => {
+  try {
+    const user = await User.findOne({ where: { email: email }})
     if (!user) {
       const error = new Error('A user with this email could not be found !')
       error.statusCode = 404
-      throw error
+      next(error)
+      return
     }
     loadedUser = user
-    return bcrypt.compare(password, user.password)
-  })
-  .then(isEqual => {
+    const isEqual = await bcrypt.compare(password, user.password)
     if (!isEqual) {
       const error = new Error('Wrong Password!')
       error.statusCode = 404
-      throw error
+      next(error)
+      return
     }
     const token = jwt.sign(
       { email: loadedUser.email, userId: loadedUser.id }, 'secret', 
       { expiresIn: '7d' }
     )
-      res.status(200).json({message: 'Successfully signed in', token: token, userId: loadedUser.id})
-  })
-  .catch(error => console.log(error))
-
+    res.status(200).json({message: 'Successfully signed in', token: token, userId: loadedUser.id})
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error)
+  }
 }
