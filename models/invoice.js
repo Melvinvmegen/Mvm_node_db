9965bc16e2d0dea87b76e8ff4c5615eb3dbd2c20
Melvin van Megen <melvin.vmegen@gmail.com>
2021-11-03 19:43:02 +0100

    UP (invoice) linted

--- models/invoice.js
+++ models/invoice.js
@@ -1,5 +1,5 @@
-'use strict';
-const { Model } = require('sequelize')
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Invoice extends Model {
@@ -7,20 +7,24 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.Revenu);
       this.belongsTo(models.Customer);
       this.hasMany(models.InvoiceItem);
+      this.hasOne(models.Quotation);
     }
-  };
-  Invoice.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    company: DataTypes.STRING,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    paymentDate: DataTypes.DATE,
-    total: DataTypes.FLOAT,
-    paid: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Invoice',
-  });
+  }
+  Invoice.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      company: DataTypes.STRING,
+      address: DataTypes.STRING,
+      city: DataTypes.STRING,
+      paymentDate: DataTypes.DATE,
+      total: DataTypes.FLOAT,
+      paid: DataTypes.BOOLEAN
+    },
+    {
+      sequelize,
+      modelName: "Invoice",
+    }
+  );
   return Invoice;
 };
