31ece6712a0f40a92ac8e0708df6896a8ebf63de
Melvin Van Megen <melvin.vmegen@gmail.com>
2022-01-26 19:36:27 +0100

    UP (revenus) add tax percentage for each month

--- controllers/revenus.js
+++ controllers/revenus.js
@@ -56,68 +56,74 @@ exports.updateRevenu = async (req, res, next) => {
     let revenu = await Revenu.findByPk(req.params.id, { include: [Credit, Cost, Invoice, Quotation] })
     const all_credits = revenu.Credits
     const mutable_credits = req.body.credits
-    const diff_credits = mutable_credits.filter(function(mutable_credit) {
-      return !all_credits.some(function(initial_credit) {
-        return initial_credit.id == mutable_credit.id
+    if (mutable_credits) {
+      const diff_credits = mutable_credits.filter(function(mutable_credit) {
+        return !all_credits.some(function(initial_credit) {
+          return initial_credit.id == mutable_credit.id
+        })
       })
-    })
-    const included_credits = mutable_credits.filter(function(mutable_credit) {
-      return all_credits.some(function(initial_credit) {
-        return initial_credit.id == mutable_credit.id
+      const included_credits = mutable_credits.filter(function(mutable_credit) {
+        return all_credits.some(function(initial_credit) {
+          return initial_credit.id == mutable_credit.id
+        })
       })
-    })
-    const createCreditsPromises = [];
-    diff_credits.forEach(credit => {
-      createCreditsPromises.push(Credit.create(credit))
-    })
-    await Promise.all(createCreditsPromises)
-    const updateCreditsPromises = [];
-
-    included_credits.forEach(credit => {
-      Credit.findByPk(credit.id).then(found_credit => {
-        if (credit._destroy) {
-          updateCreditsPromises.push(found_credit.destroy())
-        } else {
-          found_credit.reason = credit.reason,
-          found_credit.creditor = credit.creditor,
-          found_credit.total = credit.total
-          updateCreditsPromises.push(found_credit.save())
-        }
+      const createCreditsPromises = [];
+      diff_credits.forEach(credit => {
+        createCreditsPromises.push(Credit.create(credit))
       })
-    })
+      await Promise.all(createCreditsPromises)
+      const updateCreditsPromises = [];
+  
+      included_credits.forEach(credit => {
+        Credit.findByPk(credit.id).then(found_credit => {
+          if (credit._destroy) {
+            updateCreditsPromises.push(found_credit.destroy())
+          } else {
+            found_credit.reason = credit.reason,
+            found_credit.creditor = credit.creditor,
+            found_credit.total = credit.total
+            updateCreditsPromises.push(found_credit.save())
+          }
+        })
+      })
+      await Promise.all(updateCreditsPromises)
+    }
     const all_costs = revenu.Costs
     const mutable_costs = req.body.costs
-    const diff_costs = mutable_costs.filter(function(mutable_cost) {
-      return !all_costs.some(function(initial_cost) {
-        return initial_cost.id == mutable_cost.id
-      })
-    })
-    const included_costs = mutable_costs.filter(function(mutable_cost) {
-      return all_costs.some(function(initial_cost) {
-        return initial_cost.id == mutable_cost.id
-      })
-    })
-    const createCostsPromises = [];
-    diff_costs.forEach(cost => {
-      createCostsPromises.push(Cost.create(cost))
-    })
-    await Promise.all(createCostsPromises)
-    const updateCostsPromises = [];
 
-    included_costs.forEach(cost => {
-      Cost.findByPk(cost.id).then(found_cost => {
-        if (cost._destroy) {
-          updateCostsPromises.push(found_cost.destroy())
-        } else {
-          found_cost.name = cost.name,
-          found_cost.total = cost.total
-          updateCostsPromises.push(found_cost.save())
-        }
+    if (mutable_costs) {
+      const diff_costs = mutable_costs.filter(function(mutable_cost) {
+        return !all_costs.some(function(initial_cost) {
+          return initial_cost.id == mutable_cost.id
+        })
       })
-    })
+      const included_costs = mutable_costs.filter(function(mutable_cost) {
+        return all_costs.some(function(initial_cost) {
+          return initial_cost.id == mutable_cost.id
+        })
+      })
+      const createCostsPromises = [];
+      diff_costs.forEach(cost => {
+        createCostsPromises.push(Cost.create(cost))
+      })
+      await Promise.all(createCostsPromises)
+      const updateCostsPromises = [];
+  
+      included_costs.forEach(cost => {
+        Cost.findByPk(cost.id).then(found_cost => {
+          if (cost._destroy) {
+            updateCostsPromises.push(found_cost.destroy())
+          } else {
+            found_cost.name = cost.name,
+            found_cost.total = cost.total
+            found_cost.tvaAmount = cost.tvaAmount
+            updateCostsPromises.push(found_cost.save())
+          }
+        })
+      })
+      await Promise.all(updateCostsPromises)
+    }
 
-    await Promise.all(updateCostsPromises)
-    await Promise.all(updateCreditsPromises)
     revenu = await revenu.reload()
     revenu = await revenu.save()
     revenu = await Revenu.findByPk(revenu.id, { include: [Credit, Cost, Invoice, Quotation] })
@@ -141,6 +147,7 @@ exports.updateRevenu = async (req, res, next) => {
     revenu.pro = revenu_pro
     revenu.perso = revenu_perso
     revenu.total = revenu_total;
+    revenu.taxPercentage = req.body.taxPercentage
     revenu = await revenu.save()
     res.status(201).json({ message: 'Revenu updated successfully', revenu })
   } catch (error) {
