5b67a3507769139147845f701da6d1b5f5040e4c
Melvin Van Megen <melvin.vmegen@gmail.com>
2022-02-18 10:49:53 +0100

    UP (filter) refacto through util
    FIX

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,39 +1,11 @@
-const { validationResult, Result } = require('express-validator')
-const Sequelize = require('sequelize');
+const { validationResult } = require('express-validator')
 const db = require("../models/index");
 const { Customer, Invoice } = db
+const { setFilters } = require('../util/filter')
+const { notFound, validationFailed } = require('../util/errorHandler')
 
 exports.getCustomers = async (req, res, next) => {
-  const Op = Sequelize.Op
-  const queryParams = req.query
-  const offset = +queryParams.currentPage > 1 ? (+queryParams.currentPage * +queryParams.perPage) - +queryParams.perPage : 0
-  const limit = queryParams.perPage
-  const options = { 
-    limit, 
-    offset, 
-    where: [],
-    include: Invoice,
-    distinct: true
-  }
-
-  if (queryParams.name) {
-    options.where.push({[Op.or]: [
-      { firstName: {[Op.iLike]: `%${queryParams.name}%`} },
-      { lastName: {[Op.iLike]: `%${queryParams.name}%`} }
-    ]})
-  }
-
-  if (queryParams.email) {
-    options.where.push({email: {[Op.iLike]: `%${queryParams.email}%`}})
-  }
-
-  if (queryParams.city) {
-    options.where.push({city: {[Op.iLike]: `%${queryParams.city}%`}})
-  }
-
-  if (queryParams.phone) {
-    options.where.push({phone: {[Op.iLike]: `%${queryParams.phone}%`}})
-  }
+  const options = setFilters(req.query, Invoice)
 
   try {
     const customers = await Customer.findAndCountAll(options)
@@ -48,13 +20,10 @@ exports.getCustomers = async (req, res, next) => {
 }
 
 exports.showCustomer = async (req, res, next) => {
-  const id = req.params.id
   try {
-    const customer = await Customer.findByPk(id)
+    const customer = await Customer.findByPk(req.params.id)
     if (!customer) {
-      const error = new Error('Customer not found.')
-      error.statusCode = 404
-      next(error)
+      notFound(next, 'Customer')
     }
     res.status(200).json(customer)
   } catch (error) {
@@ -67,22 +36,9 @@ exports.showCustomer = async (req, res, next) => {
 
 exports.createCustomer = async (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.')
-    error.statusCode = 422
-    next(error)
-  }
+  if (!errors.isEmpty()) validationFailed(next)
   try {
-    const customer = await Customer.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      company: req.body.company,
-      email: req.body.email,
-      phone: req.body.phone,
-      address: req.body.address,
-      city: req.body.city,
-      siret: req.body.siret
-    })
+    const customer = await Customer.create(req.body)
     res.status(201).json({ message: 'Customer created successfully', customer })
   } catch (error) {
     if (!error.statusCode) {
@@ -94,22 +50,10 @@ exports.createCustomer = async (req, res, next) => {
 
 exports.updateCustomer = async (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.')
-    error.statusCode = 422
-    next(error)
-  }
-  const id = req.params.id
+  if (!errors.isEmpty()) validationFailed(next)
   try {
-    let customer = await Customer.findByPk(id)
-    customer.firstName = req.body.firstName,
-    customer.lastName = req.body.lastName,
-    customer.company = req.body.company,
-    customer.email = req.body.email,
-    customer.phone = req.body.phone,
-    customer.address = req.body.address,
-    customer.city = req.body.city,
-    customer.siret = req.body.siret
+    let customer = await Customer.findByPk(req.params.id)
+    Object.keys(req.body).forEach((key) => customer[key] = req.body[key])
     customer = await customer.save()
     res.status(201).json({ message: 'Customer updated successfully', customer })
   } catch (error) {
@@ -120,15 +64,11 @@ exports.updateCustomer = async (req, res, next) => {
   }
 }
 
-
 exports.deleteCustomer = async (req, res, next) => {
-  const id = req.params.id
   try {
-    const customer = await Customer.findByPk(id)
+    const customer = await Customer.findByPk(req.params.id)
     if (!customer) {
-      const error = new Error('Customer not found.')
-      error.statusCode = 404
-      next(error)
+      notFound(next, 'Customer')
     }
     await customer.destroy()
     res.status(200).json({message: 'Customer successfully destroyed'})
